daad9459ebf4e2769ea3686e2613abfe069c4854
Prihler <94796499+Prihler@users.noreply.github.com>
2022-11-17 21:53:02 +0100

    Update wm-program-check.sh

--- scripts/wm-program-check.sh
+++ scripts/wm-program-check.sh
@@ -2,5 +2,5 @@
 
 # starting programms on awesomewm (re)start only when not already running
 
-# solaar
-slc=$(ps aux | grep solaar > /dev/null | wc -l) && if [ $slc -eq 1 ]; then solaar -w hide &;fi
+# redshift
+rlc=$(ps aux | grep redshift > /dev/null | wc -l) && if [ $rlc -eq 1 ]; then redshift &; fi
