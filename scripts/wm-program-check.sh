54d250b004b5a359e990e2a8b5684e922c3cc6a9
Prihler <94796499+Prihler@users.noreply.github.com>
2024-02-03 00:33:27 +0100

    Update wm-program-check.sh

--- scripts/wm-program-check.sh
+++ scripts/wm-program-check.sh
@@ -3,4 +3,4 @@
 # starting programms on awesomewm (re)start only when not already running
 
 # redshift
-rlc=$(ps aux | grep redshift > /dev/null | wc -l) && if [ $rlc -eq 1 ]; then redshift &; fi
+# rlc=$(ps aux | grep redshift > /dev/null | wc -l) && if [ $rlc -eq 1 ]; then redshift &; fi
